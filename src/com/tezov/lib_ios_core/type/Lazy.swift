aace7b7b49885c52f1d708265e6357d24a7ad6c6
tezov <tezov.app@gmail.com>
2023-09-24 19:45:05 +0200

    wip

--- src/com/tezov/lib_ios_core/type/Lazy.swift
+++ src/com/tezov/lib_ios_core/type/Lazy.swift
@@ -2,19 +2,19 @@ private enum UNINITIALIZED { case SELF }
 
 @propertyWrapper public class Lazy<T: Any> {
     private var lazyValue: (() -> T)?
-    private var _value: Any = UNINITIALIZED.SELF
+    private var value: Any = UNINITIALIZED.SELF
 
     public var wrappedValue: T {
         get {
-            if _value is UNINITIALIZED {
-                _value = lazyValue!()
+            if value is UNINITIALIZED {
+                value = lazyValue!()
                 lazyValue = nil
             }
-            return _value as! T
+            return value as! T
         }
         set {
             lazyValue = nil
-            self._value = newValue
+            self.value = newValue
         }
     }
 
